46167d9a7703378276ffb40c6685c99d2eb8658b
Duan Bailey <duan.bailey@gmail.com>
2020-07-08 21:14:11 -0400

    Monsterhearts 2 - Initial feature
    - Support basic character creation, basic moves, growing up moves.
    - Support conditions and strings.
    - Add Experience on a miss setting to remind you to mark xp on a miss.

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,9 @@ var apocalypseWorld = require("./apocalypse-world")
 var burnedOver = require("./burned-over")
 var dungeonWorld = require("./dungeon-world")
 var masks = require("./masks")
+var monsterhearts = require("./monsterhearts")
 var motw = require("./motw")
 var theSprawl = require("./the-sprawl")
 var unchartedWorlds = require("./uncharted-worlds")
 
-module.exports = {apocalypseWorld, burnedOver, dungeonWorld, masks, motw, theSprawl, unchartedWorlds}
+module.exports = {apocalypseWorld, burnedOver, dungeonWorld, masks, monsterhearts, motw, theSprawl, unchartedWorlds}
